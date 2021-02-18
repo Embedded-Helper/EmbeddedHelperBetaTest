748f3ed9b0e56c67d8cddc1ad770479d55ec856d
Jacob Smith <jsmith2021@brandeis.edu>
2021-02-18 15:21:53 -0500

    added rewrite library feature

--- src/com/embeddedhelper/EmbeddedHelper.java
+++ src/com/embeddedhelper/EmbeddedHelper.java
@@ -21,8 +21,12 @@ import java.awt.Font;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
@@ -65,9 +69,9 @@ public class EmbeddedHelper implements Tool {
 			public void run() {
 
 				// show options dialogue
-				Object[] options = { "Convert this Sketch", "See an Example" };
+				Object[] options = { "Convert this Sketch", "Rewrite Library from Example", "See an Example" };
 				int result = JOptionPane.showOptionDialog(editor,
-						"Would you like to convert this program into a class or see an example?",
+						"Would you like to convert this program into a class, rewrite existing library, or see an example?",
 						"Class Generator Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options,
 						options[0]);
 
@@ -101,7 +105,7 @@ public class EmbeddedHelper implements Tool {
 								// if can't launch in browser, just display message with link
 							} catch (Exception e) {
 								JTextArea ta = new JTextArea(1, 42);
-								ta.setText("Please go to \t" + url + "\t in your browser");
+								ta.setText("Please go to following location in your file explorer\n\t" + url);
 								ta.setFont(new Font("Arial", Font.LAYOUT_LEFT_TO_RIGHT, 18));
 								ta.setWrapStyleWord(true);
 								ta.setLineWrap(true);
@@ -113,9 +117,12 @@ public class EmbeddedHelper implements Tool {
 
 						}
 					}
+				} else if (result == 1) {
+					// rewrite existing class option 1
+					classGenerated = rewriteClass(editor, tab, sketchFile, controller);
 
 					// show example sketch
-				} else if (result == 1) {
+				} else if (result == 2) {
 
 					JTextArea ta = new JTextArea(40, 90);
 					ta.setText(
@@ -139,6 +146,168 @@ public class EmbeddedHelper implements Tool {
 		}
 		// see private handleRun method of Editor class, try line 1632
 		new Thread(new LibHandler()).start();
+	}
+
+	/**
+	 * Converts the program into a generated class
+	 * 
+	 * @param editor     Arduino object used to save file
+	 * @param tab        Arduino Object used to get the contents of this tab
+	 * @param sketchFile Arduino Object used to get file name
+	 * @param controller Arduino object used to display error message
+	 * @return whether the class was generated
+	 */
+	private static boolean rewriteClass(Editor editor, EditorTab tab, SketchFile sketchFile,
+			SketchController controller) {
+
+		editor.handleSave(true);
+
+		// get the file name and contents of the sketch
+		String[] sketchInfo = getNameContentsPath(tab, sketchFile);
+		String className = sketchInfo[0];
+		String contents = sketchInfo[1];
+		String parentPath = sketchInfo[3];
+
+		System.out.println(className + "\t" + parentPath);
+
+		// compile sketch to check for errors
+		editor.statusNotice(tr("Compiling sketch..."));
+		if (failedToCompile(editor, controller)) {
+			return false;
+		}
+		// parse the sketch into format used to generate arduino classes
+		editor.statusNotice(tr("Rewriting Library Files..."));
+		Path parentPath2 = Paths.get(parentPath).getParent().getParent();
+		String classPath = parentPath2.toAbsolutePath().toString();
+		className = parentPath2.getFileName().toString();
+
+		String sep = java.io.File.separator;
+
+		SketchParser parser = new SketchParser(contents);
+		ArduinoClassContainer cont = parser.getContainer(className, true);
+		String newHeader = cont.getHeader();
+		String newBody = cont.getBody();
+		String newExample = cont.getExample();
+
+		String headerPath = classPath + sep + className + ".h";
+		String headerContents = readFile(headerPath);
+		String originalHeaderContents = headerContents;
+
+		headerContents = insertSection2(headerContents, newHeader, ";", "public:", ";", "public:");
+		headerContents = insertSection(headerContents, newHeader, "();", "}", "begin();", "}");
+		headerContents = replaceHeaderComment(contents, headerContents);
+		System.out.println(newHeader+"::\n:;"+headerContents);
+
+		writeFile(headerPath, headerContents);
+
+		// compile sketch to check for errors
+		editor.statusNotice(tr("Compiling sketch..."));
+		if (failedToCompile(editor, controller)) {
+			writeFile(headerPath, originalHeaderContents);
+			return false;
+		}
+
+		String bodyPath = classPath + sep + className + ".cpp";
+		String bodyContents = readFile(bodyPath);
+		writeFile(bodyPath, replaceHeaderComment(contents, bodyContents));
+
+		System.out.println("CLASSGEN: The class files were edited, please close this window");
+		System.out.println("\t and to see them, go to\n\t\t" + parentPath2);
+
+		return true;
+
+	}
+
+	private static String insertSection2(String input, String insert, String inputStartToken, String inputEndToken,
+			String insertStartToken, String insertEndToken) {
+		
+		try {
+		// find start position in input from first occurence of startToken before the
+		// endToken
+		int inputEnd = input.indexOf(inputEndToken);
+		
+		int inputStart = input.lastIndexOf(inputStartToken, inputEnd);
+
+		int insertEnd = insert.indexOf(insertEndToken);
+		int insertStart = insert.lastIndexOf(insertStartToken, insertEnd);
+		
+		
+
+
+		return input.substring(0, inputStart + inputStartToken.length())
+				+ insert.substring(insertStart + insertStartToken.length(), insertEnd)
+				+ input.substring(inputEnd, input.length());
+		
+		}catch (Exception e) {
+			System.out.println(e.getClass().toString());
+			return input;
+		}
+
+	}
+
+	private static String insertSection(String input, String insert, String inputStartToken, String inputEndToken,
+			String insertStartToken, String insertEndToken) {
+
+		int inputStart = input.lastIndexOf(inputStartToken);
+		int inputEnd = input.lastIndexOf(inputEndToken);
+
+		int insertStart = insert.indexOf(insertStartToken);
+		int insertEnd = insert.indexOf(insertEndToken);
+
+		if (inputStart == -1 || inputEnd == -1 || insertStart == -1 || insertEnd == -1) {
+			return input;
+		}
+
+		return input.substring(0, inputStart + inputStartToken.length())
+				+ insert.substring(insertStart + insertStartToken.length(), insertEnd)
+				+ input.substring(inputEnd, input.length());
+
+	}
+
+	private static String replaceHeaderComment(String insert, String input) {
+		int startCommentInput = input.indexOf("/*");
+		int endCommentInput = input.indexOf("*/");
+		int startCommentInsert = insert.indexOf("/*");
+		int endCommentInsert = insert.indexOf("*/");
+		if (startCommentInsert == -1 || endCommentInsert == -1 || startCommentInput == -1 || endCommentInput == -1) {
+			return null;
+		}
+		return input.substring(0, startCommentInput) + insert.substring(startCommentInsert, endCommentInsert)
+				+ input.substring(endCommentInput, input.length());
+	}
+
+	private static String readFile(String filePath) {
+		File f = new File(filePath);
+		String contents = "";
+		// create scanner to iterate over file
+		// set private variable to tell whether file was found
+		Scanner reader = null;
+		try {
+			reader = new Scanner(f);
+
+		} catch (Exception e) {
+			System.out.println("ERROR, file not found");
+			return null;
+		}
+		// load contents string
+		while (reader.hasNextLine()) {
+			contents += reader.nextLine() + "\r\n";
+		}
+		reader.close();
+		return contents;
+	}
+
+	private static boolean writeFile(String filePath, String contents) {
+		try {
+			PrintStream output = new PrintStream(new File(filePath));
+			output.print(contents);
+			output.close();
+		} catch (Exception e) {
+			System.out.println("Error, file not found");
+			return false;
+		}
+		return true;
+
 	}
 
 	/**
